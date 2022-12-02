c74df7a230758db2b8b66b38a5a56e0487eefcdc
Christian Aquino <cjfaquino@gmail.com>
2022-12-01 23:49:14 -0800

    add extra personal inputs

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -7,12 +7,25 @@ export default class Overview extends Component {
     return (
       <div id='overview'>
         <div className='overview-personal'>
-          <div className='overview-name'>{personal.name}</div>
-          <div className='overview-contact'>
+          <div className='overview-name'>
+            {personal.firstName} {personal.lastName}
+          </div>
+          <div className='overview-card'>
             <div className='overview-address'>{personal.address}</div>
-            <div className='overview-phone'>{personal.phone}</div>
+            <div className='overview-contact'>
+              <div className='overview-phone'>{personal.phone}</div>
+              <div className='overview-email'>{personal.email}</div>
+            </div>
           </div>
         </div>
+
+        {personal.description !== '' ? (
+          <div>
+            <hr className='overview-lines' />
+            <h3 className='overview-desc-label'>Description</h3>
+            <div className='overview-description'>{personal.description}</div>
+          </div>
+        ) : null}
 
         {!(experience.array.length === 0) ? (
           <div>
@@ -20,13 +33,17 @@ export default class Overview extends Component {
             <h3 className='overview-exp'>Experience</h3>
             {experience.array.map((item) => (
               <div key={item.uuid} className='experience-item'>
-                <div className='experience-dates'>
-                  {item.from} - {item.until}
-                </div>
+                {item.from !== '' || item.until !== '' ? (
+                  <div className='experience-dates'>
+                    {item.from} - {item.until}
+                  </div>
+                ) : null}
                 <div className='experience-position'>{item.position}</div>
-                <div className='experience-location'>
-                  {item.name} - {item.city}, {item.state}
-                </div>
+                {item.name !== '' || item.city !== '' || item.state !== '' ? (
+                  <div className='experience-location'>
+                    {item.name} - {item.city}, {item.state}
+                  </div>
+                ) : null}
               </div>
             ))}
           </div>
@@ -38,16 +55,20 @@ export default class Overview extends Component {
             <h3 className='overview-edu'>Education</h3>
             {education.array.map((item) => (
               <div key={item.uuid} className='education-item'>
-                <div className='education-name'>
-                  {item.name}
-                  {', '}
-                  <span className='education-location'>
-                    {item.city}, {item.state}
-                  </span>
-                </div>
-                <div className='education-degree'>
-                  {item.degree} - {item.subject}
-                </div>
+                {item.name !== '' || item.city !== '' ? (
+                  <div className='education-name'>
+                    {item.name}
+                    {', '}
+                    <span className='education-location'>
+                      {item.city}, {item.state}
+                    </span>
+                  </div>
+                ) : null}
+                {item.degree !== '' || item.subject !== '' ? (
+                  <div className='education-degree'>
+                    {item.degree} - {item.subject}
+                  </div>
+                ) : null}
               </div>
             ))}
           </div>
