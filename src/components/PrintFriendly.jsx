ab96d774d3891d0312ef924be25d63abf2ca795d
Christian Aquino <cjfaquino@gmail.com>
2022-12-03 20:53:05 -0800

    fix print view

--- src/components/PrintFriendly.jsx
+++ src/components/PrintFriendly.jsx
@@ -6,20 +6,22 @@ export default class PrintFriendly extends Component {
     const { object, editFormData } = this.props;
     return (
       <div className='print-menu'>
-        <button
-          type='button'
-          className='edit-button no-print'
-          onClick={editFormData}
-        >
-          Edit
-        </button>
-        <button
-          type='button'
-          className='submit-button no-print'
-          onClick={() => window.print()}
-        >
-          Print friendly version
-        </button>
+        <div className='print-buttons'>
+          <button
+            type='button'
+            className='edit-button no-print'
+            onClick={editFormData}
+          >
+            Edit
+          </button>
+          <button
+            type='button'
+            className='submit-button no-print'
+            onClick={() => window.print()}
+          >
+            Print friendly version
+          </button>
+        </div>
 
         <Overview object={object} />
       </div>
